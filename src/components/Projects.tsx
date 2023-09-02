122de13aead20c52afdd5a67a98b8d489f3859a4
sandeep reddy <sandeepreddyr12@gmail.com>
2023-09-02 14:22:23 +0530

    added discription

--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -1,5 +1,4 @@
 import React from "react";
-import Heading from "./Heading";
 import Card from "./Card";
 import { data } from "./projectsData";
 
@@ -7,10 +6,20 @@ import { data } from "./projectsData";
 
 const Projects = () => {
   return (
-    <div id="projects" className="w-screen mx-auto px-12  mb-16 py-20">
-      <div className="text-4xl  font-bold border-b-4 border-gray-500 p-2 inline">
+    <div id="projects" className="w-screen mx-auto px-3  md:px-6   mb-16 py-20">
+      <div className="text-3xl  lg:text-4xl  font-bold border-b-4 border-green-500 p-2 ml-4 inline">
         Personal Projects
       </div>{' '}
+      <p className="text-secondary font-mono md:ml-10  my-6 w-full lg:w-[80%]">
+        Note : {'  '}Regrettably, a few of my personal websites are currently
+        inaccessible due to a combination of factors, including the expiration
+        of the free tiers on Heroku, AWS S3 and other services, as well as my extensive use of
+        beta/unstable features. However, I&apos;ve taken steps to ensure that
+        each project remains accessible by providing GitHub repository links or
+        pre-recorded videos. Almost always, my personal projects are unique and
+        offer a very good learning curve. Decent-Bet is my current best
+        project.{' '}
+      </p>
       <div className="grid mt-20 gap-28 xl:gap-0 xl:gap-y-10 md:grid-cols-2 lg:grid-cols-3 place-items-center lg:px-10">
         {data.map((el) => (
           <Card
